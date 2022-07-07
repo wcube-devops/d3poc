23e552ac4d0f26ee78ab98858fd8e1e2a488d827
jonatanlaksamanapurnomo <jonatanlaksamanapurnomo@gmail.com>
2022-07-07 23:23:42 +0700

    initial commit mike poc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Papa from "papaparse"
+import {ForceGraph} from "./components/forceGraph";
 
 function App() {
+  const [parsedData, setData] = useState({})
+  const [isLoading, setLoading] = useState(true)
+  useEffect(() => {
+    async function getData() {
+      let data = []
+      const set = new Set()
+      const response = await fetch('../data/test.csv')
+      const reader = response.body.getReader()
+      const result = await reader.read() // raw array
+      const decoder = new TextDecoder('utf-8')
+      const csv = decoder.decode(result.value) // the csv text
+      const results = Papa.parse(csv) // ob
+      let idx = 0
+      let map = {}
+      let links = []
+      let dataParse = {}
+      results.data.forEach(subArr => {
+        for (let i = 0; i < subArr.length - 1; i++) {
+          set.add(subArr[i])
+        }
+      })
+
+      set.forEach((val) => {
+        map[`${val}`] = idx
+        let obj = {id: idx++, name: val}
+        data.push(obj)
+      })
+
+      results.data.forEach(subArr => {
+        for (let i = 0; i < subArr.length - 1; i += 2) {
+          let key = subArr[i]
+          let obj = {}
+          obj.source = map[key]
+          obj.target = map[subArr[i + 1]]
+          obj.width = parseInt(subArr[i + 2])
+          links.push(obj)
+        }
+      })
+      dataParse.nodes = data
+      dataParse.links = links
+      setData(dataParse)
+      setLoading(false)
+    }
+
+    getData()
+  }, [])
+
+  const hoverToolTip = React.useCallback((node) => {
+    return `<div>     
+      <b>${node.name}</b>
+    </div>`;
+  }, []);
+  if (isLoading) {
+    return <p>loading</p>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ForceGraph linksData={parsedData.links} nodesData={parsedData.nodes} nodeHoverTooltip={hoverToolTip}/>
     </div>
   );
 }
